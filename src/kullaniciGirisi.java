f33b5d14e50ca976794385f6ba022e99b90d8d3f
Fatih Turker <fthturker80@gmail.com>
2022-10-11 21:45:08 +0300

    ft

--- src/kullaniciGirisi.java
+++ src/kullaniciGirisi.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class kullaniciGirisi {
     public static void main(String[] args) {
         /*
@@ -7,5 +9,30 @@ public class kullaniciGirisi {
         şifreler aynı ise ekrana "Şifre oluşturulamadı,
         lütfen başka şifre giriniz." sorun yoksa "Şifre oluşturuldu" yazan programı yazınız.
          */
+        String kullaniciAdi, sifre, secim, yeniSifre;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Kullanıcı adınızı giriniz: ");
+        kullaniciAdi = input.nextLine();
+        System.out.print("Şifrenizi giriniz: ");
+        sifre = input.nextLine();
+
+        if (kullaniciAdi.equals("patika") && sifre.equals("java123"))
+            System.out.print("Sisteme giriş yaptınız!");
+        else if (!(sifre.equals("java123"))) {
+            System.out.println("Şifre hatalı! Şifreyi değiştirmek ister misiniz? E/H");
+            System.out.print("Seçiminiz: ");
+            secim = input.nextLine();
+            if (secim.equals("E")) {
+                System.out.print("Yeni şifreniz:");
+                yeniSifre = input.nextLine();
+                if (yeniSifre.equals("java123")) {
+                    System.out.println("Şifre oluşturulamadı, lütfen başka şifre giriniz.");
+                } else
+                    System.out.print("Şifre oluşturuldu.");
+            }
+        }
+        else
+            System.out.print("Böyle bir kullanıcı bulunamadı.");
     }
 }
